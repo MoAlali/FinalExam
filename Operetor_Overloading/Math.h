39ebb37857ef5b2ffc5f39663d8d0939db2c0c5b
Mohammed alali <84029535+MoAlali@users.noreply.github.com>
2023-02-23 21:30:05 +0300

    Updates

---
+++ Operetor_Overloading/Math.h
@@ -0,0 +1,16 @@
+class Math
+{
+private:
+    /* data */
+public:
+    Math(/* args */);
+    ~Math();
+};
+
+Math::Math(/* args */)
+{
+}
+
+Math::~Math()
+{
+}
---
+++ Operetor_Overloading/main.cpp
@@ -0,0 +1,12 @@
+#include "Math.h"
+#include <iostream>
+using namespace std;
+ int main(){
+   int num1 = 4 , num2 = 6;
+   cout << num1 + num2 << endl;
+
+
+
+   Math m1 , m2;
+
+ }
